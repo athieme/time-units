17076ca5cfb92ee949db3c7326788c0be425139c
athieme <Gr8v1yard>
2014-02-05 09:51:58 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Convert between different units of time (hours, minutes, seconds, etc)
---
+++ test/index.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2013 Alex Thieme  All rights reserved.
+ */
+
+'use strict';
+
+var logger = require('winston');
+var should = require("should");
+
+describe('Session tests' , function () {
+
+    before(function (done) {
+        // do something
+
+        done();
+    });
+
+    it('test something...' , function (done) {
+      done();
+    });
+
+    after(function () {
+        // do something
+    });
+});
+
+
