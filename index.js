44ca1df7b673e7eb0c75b3136606599a4e25b601
athieme <Gr8v1yard>
2014-02-05 14:44:05 -0800

    added days and hours conversions updated tests added some TODOs

--- index.js
+++ index.js
@@ -8,8 +8,26 @@ module.exports = function (number) {
 
     var result = {};
 
+    // TODO should be able to define the units and functions once and go up and down
+    // TODO should be able to go up and down, skipping steps in between
     var convert = {
+        d : {
+            h : function (number) {
+                return number * 24;
+            }
+        } ,
+        h : {
+            d : function (number) {
+                return number / 24;
+            } ,
+            m : function (number) {
+                return number * 60;
+            }
+        } ,
         m : {
+            h : function (number) {
+                return number / 60;
+            } ,
             s : function (number) {
                 return number * 60;
             }
@@ -17,7 +35,7 @@ module.exports = function (number) {
         s : {
             m : function (number) {
                 return number / 60;
-            },
+            } ,
             ms : function (number) {
                 return number * 1000;
             }
@@ -31,17 +49,31 @@ module.exports = function (number) {
 
     var state = { number : number};
 
+    /**
+     * Specify the number's time unit
+     * @param from_unit the number's time unit
+     * @returns {result}
+     */
     result.from = function (from_unit) {
         state.from_unit = from_unit;
         return this;
     };
 
+    /**
+     * Convert to the specified time unit
+     * @param to_unit   the time unit to convert to
+     * @returns {result}
+     */
     result.to = function (to_unit) {
         state.number = convert[state.from_unit][to_unit](state.number);
         state.from_unit = to_unit;
         return this;
     };
 
+    /**
+     * Returns the converted number
+     * @returns {*}
+     */
     result.value = function () {
         return state.number;
     };
--- test/index.js
+++ test/index.js
@@ -17,16 +17,69 @@ describe('Session tests' , function () {
     });
 
     it('between milliseconds and seconds' , function (done) {
-        convert(3000).from('ms').to('s').value().should.equal(3);
-        convert(3).from('s').to('ms').value().should.equal(3000);
-        convert(3000).from('ms').to('s').to('ms').value().should.equal(3000);
+        convert(3000)
+            .from('ms')
+            .to('s')
+            .value().should.equal(3);
+        convert(3).from('s')
+            .to('ms')
+            .value().should.equal(3000);
+        convert(3000).from('ms')
+            .to('s')
+            .to('ms')
+            .value().should.equal(3000);
         done();
     });
 
-    it('between econds and minutes' , function (done) {
-        convert(120).from('s').to('m').value().should.equal(2);
-        convert(2).from('m').to('s').value().should.equal(120);
-        convert(120).from('s').to('m').to('s').value().should.equal(120);
+    it('between seconds and minutes' , function (done) {
+        convert(120).from('s')
+            .to('m')
+            .value().should.equal(2);
+        convert(2).from('m')
+            .to('s')
+            .value().should.equal(120);
+        convert(120).from('s')
+            .to('m')
+            .to('s')
+            .value().should.equal(120);
+        done();
+    });
+
+    it('between minutes and hours' , function (done) {
+        convert(120).from('m')
+            .to('h')
+            .value().should.equal(2);
+        convert(2).from('h')
+            .to('m')
+            .value().should.equal(120);
+        convert(120).from('m')
+            .to('h')
+            .to('m')
+            .value().should.equal(120);
+        done();
+    });
+
+    it('between hours and days' , function (done) {
+        convert(72).from('h')
+            .to('d')
+            .value().should.equal(3);
+        convert(3).from('d')
+            .to('h')
+            .value().should.equal(72);
+        convert(72).from('h')
+            .to('d')
+            .to('h')
+            .value().should.equal(72);
+        done();
+    });
+
+    it('between all' , function (done) {
+        convert(172800000).from('ms')
+            .to('s')
+            .to('m')
+            .to('h')
+            .to('d')
+            .value().should.equal(2);
         done();
     });
 
