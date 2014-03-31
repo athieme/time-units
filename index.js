16e68c1fc3a33b6dd07ba8bc201e2c7fd5eed856
athieme <Gr8v1yard>
2014-03-31 11:07:48 -0700

    added (skipped) test for converting between all, skipping in between (currently not supported) reformatted

--- index.js
+++ index.js
@@ -6,78 +6,78 @@ var logger = require('winston');
 
 module.exports = function (number) {
 
-    var result = {};
+  var result = {};
 
-    // TODO should be able to define the units and functions once and go up and down
-    // TODO should be able to go up and down, skipping steps in between
-    var convert = {
-        d : {
-            h : function (number) {
-                return number * 24;
-            }
-        } ,
-        h : {
-            d : function (number) {
-                return number / 24;
-            } ,
-            m : function (number) {
-                return number * 60;
-            }
-        } ,
-        m : {
-            h : function (number) {
-                return number / 60;
-            } ,
-            s : function (number) {
-                return number * 60;
-            }
-        } ,
-        s : {
-            m : function (number) {
-                return number / 60;
-            } ,
-            ms : function (number) {
-                return number * 1000;
-            }
-        } ,
-        ms : {
-            s : function (number) {
-                return number / 1000;
-            }
-        }
-    };
+  // TODO should be able to define the units and functions once and go up and down
+  // TODO should be able to go up and down, skipping steps in between
+  var convert = {
+    d : {
+      h : function (number) {
+        return number * 24;
+      }
+    },
+    h : {
+      d : function (number) {
+        return number / 24;
+      },
+      m : function (number) {
+        return number * 60;
+      }
+    },
+    m : {
+      h : function (number) {
+        return number / 60;
+      },
+      s : function (number) {
+        return number * 60;
+      }
+    },
+    s : {
+      m : function (number) {
+        return number / 60;
+      },
+      ms : function (number) {
+        return number * 1000;
+      }
+    },
+    ms : {
+      s : function (number) {
+        return number / 1000;
+      }
+    }
+  };
 
-    var state = { number : number};
+  var state = { number : number};
 
-    /**
-     * Specify the number's time unit
-     * @param from_unit the number's time unit
-     * @returns {result}
-     */
-    result.from = function (from_unit) {
-        state.from_unit = from_unit;
-        return this;
-    };
+  /**
+   * Specify the number's time unit
+   * @param from_unit the number's time unit
+   * @returns {result}
+   */
+  result.from = function (from_unit) {
+    state.from_unit = from_unit;
+    return this;
+  };
 
-    /**
-     * Convert to the specified time unit
-     * @param to_unit   the time unit to convert to
-     * @returns {result}
-     */
-    result.to = function (to_unit) {
-        state.number = convert[state.from_unit][to_unit](state.number);
-        state.from_unit = to_unit;
-        return this;
-    };
+  /**
+   * Convert to the specified time unit
+   * @param to_unit   the time unit to convert to
+   * @returns {result}
+   */
+  result.to = function (to_unit) {
+    state.number = convert[state.from_unit][to_unit](state.number);
+    state.from_unit = to_unit;
+    return this;
+  };
 
-    /**
-     * Returns the converted number
-     * @returns {*}
-     */
-    result.value = function () {
-        return state.number;
-    };
+  /**
+   * Returns the converted number
+   * @returns {*}
+   */
+  result.value = function () {
+    return state.number;
+  };
 
-    return result;
+  return result;
 }
 
--- test/index.js
+++ test/index.js
@@ -8,84 +8,91 @@ var logger = require('winston');
 var should = require('should');
 var convert = require('..');
 
-describe('Session tests' , function () {
+describe('Session tests', function () {
 
-    before(function (done) {
-        // do something
+  before(function (done) {
+    // do something
 
-        done();
-    });
+    done();
+  });
 
-    it('between milliseconds and seconds' , function (done) {
-        convert(3000)
-            .from('ms')
-            .to('s')
-            .value().should.equal(3);
-        convert(3).from('s')
-            .to('ms')
-            .value().should.equal(3000);
-        convert(3000).from('ms')
-            .to('s')
-            .to('ms')
-            .value().should.equal(3000);
-        done();
-    });
+  it('between milliseconds and seconds', function (done) {
+    convert(3000)
+      .from('ms')
+      .to('s')
+      .value().should.equal(3);
+    convert(3).from('s')
+      .to('ms')
+      .value().should.equal(3000);
+    convert(3000).from('ms')
+      .to('s')
+      .to('ms')
+      .value().should.equal(3000);
+    done();
+  });
 
-    it('between seconds and minutes' , function (done) {
-        convert(120).from('s')
-            .to('m')
-            .value().should.equal(2);
-        convert(2).from('m')
-            .to('s')
-            .value().should.equal(120);
-        convert(120).from('s')
-            .to('m')
-            .to('s')
-            .value().should.equal(120);
-        done();
-    });
+  it('between seconds and minutes', function (done) {
+    convert(120).from('s')
+      .to('m')
+      .value().should.equal(2);
+    convert(2).from('m')
+      .to('s')
+      .value().should.equal(120);
+    convert(120).from('s')
+      .to('m')
+      .to('s')
+      .value().should.equal(120);
+    done();
+  });
 
-    it('between minutes and hours' , function (done) {
-        convert(120).from('m')
-            .to('h')
-            .value().should.equal(2);
-        convert(2).from('h')
-            .to('m')
-            .value().should.equal(120);
-        convert(120).from('m')
-            .to('h')
-            .to('m')
-            .value().should.equal(120);
-        done();
-    });
+  it('between minutes and hours', function (done) {
+    convert(120).from('m')
+      .to('h')
+      .value().should.equal(2);
+    convert(2).from('h')
+      .to('m')
+      .value().should.equal(120);
+    convert(120).from('m')
+      .to('h')
+      .to('m')
+      .value().should.equal(120);
+    done();
+  });
 
-    it('between hours and days' , function (done) {
-        convert(72).from('h')
-            .to('d')
-            .value().should.equal(3);
-        convert(3).from('d')
-            .to('h')
-            .value().should.equal(72);
-        convert(72).from('h')
-            .to('d')
-            .to('h')
-            .value().should.equal(72);
-        done();
-    });
+  it('between hours and days', function (done) {
+    convert(72).from('h')
+      .to('d')
+      .value().should.equal(3);
+    convert(3).from('d')
+      .to('h')
+      .value().should.equal(72);
+    convert(72).from('h')
+      .to('d')
+      .to('h')
+      .value().should.equal(72);
+    done();
+  });
 
-    it('between all' , function (done) {
-        convert(172800000).from('ms')
-            .to('s')
-            .to('m')
-            .to('h')
-            .to('d')
-            .value().should.equal(2);
-        done();
-    });
+  it('between all', function (done) {
+    convert(172800000).from('ms')
+      .to('s')
+      .to('m')
+      .to('h')
+      .to('d')
+      .value().should.equal(2);
+    done();
+  });
 
-    after(function () {
-        // do something
-    });
+  it.skip('between all, skipping in between', function (done) {
+    convert(172800000).from('ms')
+      .to('d')
+      .value().should.equal(2);
+    done();
+  });
+
+  after(function () {
+    // do something
+  });
 });
 
 
